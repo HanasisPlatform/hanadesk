5066f3e107d1895cff57cfa2928aa22473c15524
hdkim <hdkim@hanasis.com>
2025-06-27 12:23:38 +0900

    run_cs.sh 및 run_member.sh에서 주석 처리된 cargo build 명령어를 활성화하여 flutter 실행 프로세스를 개선함

--- flutter/run_member.sh
+++ flutter/run_member.sh
@@ -7,5 +7,6 @@ export LLVM_HOME="${LLVM_PATH:-C:/LLVM}"
 ~/.cargo/bin/flutter_rust_bridge_codegen --rust-input ../src/flutter_ffi.rs --dart-output ./lib/generated_bridge.dart --c-output ./windows/runner/bridge_generated.h --llvm-path "${LLVM_PATH:-C:/LLVM}"
 # call `flutter clean` if cargo build fails
 # export LLVM_HOME=/Library/Developer/CommandLineTools/usr/
-# cargo build --features flutter
+
+cargo build --features flutter
 flutter run $@ --dart-define=FLAVOR=member lib/main_member.dart
